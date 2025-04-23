173ad1000d5edcca8f5e28a680735d8a074ba243
Zenslim <zenslim2010@gmail.com>
2025-04-23 18:29:53 -0400

    Create planetPrompts.js

---
+++ data/planetPrompts.js
@@ -0,0 +1,72 @@
+export const planetPrompts = [
+  {
+    name: "Earth",
+    image: "earth.png",
+    prompts: [
+      "What dream have you abandoned?",
+      "Where does your soul feel most rooted?",
+      "What legacy are you planting?",
+      "What part of you longs to grow slowly?"
+    ]
+  },
+  {
+    name: "Mars",
+    image: "mars.png",
+    prompts: [
+      "What fire are you afraid to light?",
+      "What would courage do now?",
+      "Where are you holding back your power?",
+      "What battle are you tired of avoiding?"
+    ]
+  },
+  {
+    name: "Moon",
+    image: "moon.png",
+    prompts: [
+      "Who are you beneath it all?",
+      "What emotions do you hide from others?",
+      "What truth is asking to be felt?",
+      "What if your softness is your strength?"
+    ]
+  },
+  {
+    name: "Jupiter",
+    image: "jupiter.png",
+    prompts: [
+      "What truth are you expanding into?",
+      "Where are you being invited to grow?",
+      "What belief could change your life?",
+      "What possibility feels too big to believe?"
+    ]
+  },
+  {
+    name: "Pluto",
+    image: "pluto.png",
+    prompts: [
+      "What’s dying to be expressed?",
+      "What part of you needs to dissolve?",
+      "What transformation are you resisting?",
+      "What fear is guarding your rebirth?"
+    ]
+  },
+  {
+    name: "Saturn",
+    image: "saturn.png",
+    prompts: [
+      "What cycle are you ready to close?",
+      "What boundary do you need to honor?",
+      "Where is structure holding you back?",
+      "What lesson keeps returning?"
+    ]
+  },
+  {
+    name: "Populated",
+    image: "populated.png",
+    prompts: [
+      "What part of you is quietly thriving?",
+      "What are you already doing right?",
+      "Where are you more alive than you admit?",
+      "What if you're more ready than you think?"
+    ]
+  }
+];
