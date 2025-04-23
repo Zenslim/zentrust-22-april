6601e97fe426f4dcd8352b4320f64ee3c18af11f
Zenslim <zenslim2010@gmail.com>
2025-04-23 12:55:59 -0400

    Update CelestialBackground.jsx

--- components/CelestialBackground.jsx
+++ components/CelestialBackground.jsx
@@ -9,10 +9,11 @@ export default function CelestialBackground() {
         loop
         playsInline
         className="w-full h-full object-cover"
+        style={{ pointerEvents: 'none' }}
       >
         <source src="/space.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
-  )
+  );
 }
