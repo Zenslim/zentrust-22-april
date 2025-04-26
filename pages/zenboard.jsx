3dbc0bd24536f304d935032ca46efef314db69a0
Zenslim <zenslim2010@gmail.com>
2025-04-26 16:13:16 -0400

    Update zenboard.jsx

--- pages/zenboard.jsx
+++ pages/zenboard.jsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useState, useEffect } from 'react';
 import AmbientAudio from '@/components/AmbientAudio';
 import CelestialBackground from '@/components/CelestialBackground';
